8424994ba60cad3ffe2a4f4188c489260654e1ba
Abigail Cabunoc <abigail.cabunoc@gmail.com>
2014-10-21 15:36:12 -0400

    Home page

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.render('index.jade');
 })
 
 app.listen(app.get('port'), function() {
