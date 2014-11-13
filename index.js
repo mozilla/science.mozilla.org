d2fc166de6fb44fee080a9ca6c936b8c93f38b8f
Abigail Cabunoc <abigail.cabunoc@gmail.com>
2014-11-13 15:42:14 -0500

    bringing in static pages

--- index.js
+++ index.js
@@ -7,8 +7,33 @@ app.set('views', __dirname + '/views');
 
 app.get('/', function(request, response) {
   response.render('index.jade');
-})
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
+
+
+app.get('/collaborate', function(request, response) {
+  response.render('collaborate/collaborate.jade');
+});
+
+app.get('/collaborate/about', function(request, response) {
+  response.render('collaborate/about.jade');
+});
+
+app.get('/about', function(request, response) {
+  response.render('about.jade');
+});
+
+app.get('/education', function(request, response) {
+  response.render('education.jade');
+});
+
+app.get('/community', function(request, response) {
+  response.render('community.jade');
+});
+
+app.get('/blog', function(request, response) {
+  response.render('blog.jade');
+});
