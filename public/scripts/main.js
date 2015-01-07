323f75e60ff6619d196deabcc7553a7adfa2c298
Abigail Cabunoc <abigail.cabunoc@gmail.com>
2015-01-07 10:16:42 -0500

    script for menu

---
+++ public/scripts/main.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+(function(window, document, undefined){
+
+
+
+  $(document).ready(function(){
+    $('#menu-button').click(function(){
+      $('.pure-menu ul').toggleClass('display');
+    })
+  });
+
+})(window, document);
+
