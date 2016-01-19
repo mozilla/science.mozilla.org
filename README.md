528b7987f7bfb8d720057711a2725f2e17fd2653
Abigail Cabunoc <abigail.cabunoc@gmail.com>
2016-01-18 21:28:52 -0500

    Fix build! * added webpack + jsx-loader + npm-run-all * deleted redundant files

--- README.md
+++ README.md
@@ -17,4 +17,4 @@ Find a problem with any of Mozilla Science's websites?  Open a new issue in the 
 2. Install the Mozilla Science website's Node dependencies: `npm install`
 3. Copy the configuration template to its expected location: `cp env.dist .env`
 4. Open .env in your favourite text editor and ensure that your PORT, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET and GITHUB_TOKEN environment variables are set to the correct values. PORT can be any available port.
-4. Run `node app.js`, and open up `http://localhost:5000/` in your favourite web browser!
+4. Run `npm start`, and open up `http://localhost:5000/` in your favourite web browser!
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+
+var path = require('path');
+var webpack = require('webpack');
+
+var IMPORT_ES5_SHIM = 'imports?shim=es5-shim/es5-shim&' +
+  'sham=es5-shim/es5-sham';
+
+module.exports = {
+  entry: './src/react-main.jsx',
+
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js',
+    path: path.join('public', 'js'),
+    publicPath: '/js/'
+  },
+  externals: {
+    App: true
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx$/,
+      loaders: ['jsx-loader']
+    }, {
+      test: require.resolve('react'),
+      loader: IMPORT_ES5_SHIM
+    }]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    })
+  ]
+};
