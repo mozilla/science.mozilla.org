3f3da6485bebd819eb7e2166ee61b9ffc1585243
Gavin Lazar Suntop <gavin@gsuntop.com>
2016-04-05 14:24:07 -0700

    adding footer links

--- app/js/components/footer.jsx
+++ app/js/components/footer.jsx
@@ -20,12 +20,12 @@ export default React.createClass({
           </div>
         </div>
         <ul className="badged-links nav nav-inline">
-          <a className="nav-link" href="#">Twitter</a>
-          <a className="nav-link" href="#">IRC</a>
-          <a className="nav-link" href="#">Email Us</a>
-          <a className="nav-link" href="#">Terms of Use</a>
-          <a className="nav-link" href="#">Privacy Policy</a>
-          <a className="nav-link" href="#">Code of Conduct</a>
+          <a className="nav-link" target="_blank" href="https://twitter.com/MozillaScience">Twitter</a>
+          <a className="nav-link" href="irc://irc.mozilla.org/sciencelab">IRC</a>
+          <a className="nav-link" href="#TODO">Email Us</a>
+          <a className="nav-link" target="_blank" href="https://www.mozilla.org/en-US/about/legal/terms/mozilla/">Terms of Use</a>
+          <a className="nav-link" target="_blank" href="https://www.mozilla.org/en-US/privacy/">Privacy Policy</a>
+          <a className="nav-link" href="#TODO">Code of Conduct</a>
         </ul>
         <div className="row">
           <div className="col-sm-4"><h4>Mozilla</h4></div>
