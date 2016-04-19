9e2d839ee0bc484e6adcf87af7fb32862aeb7830
Gavin Lazar Suntop <gavin@gsuntop.com>
2016-04-19 11:28:00 -0700

    adding events & study groups cta

--- app/react/pages/home/home.jsx
+++ app/react/pages/home/home.jsx
@@ -17,7 +17,7 @@ export default React.createClass({
           item2={{text: `A community of researchers advocating for openness and collaboration`, icon: `/assets/img/icon-community.svg`}}
           item3={{text: `Open Science: A way for science to achieve its fullest potential`, icon: `/assets/img/icon-open.svg`}}></ThreeUp>
 
-        <div className="fellow-cta">
+        <div className="fellow-cta m-b-3">
           <img src="/assets/img/icon-drop.svg" className="sprinkle"/>
           <img src="/assets/img/icon-droptwo.svg" className="sprinkle"/>
 
@@ -26,6 +26,29 @@ export default React.createClass({
               <h2>Apply to be a Fellow</h2>
               <p>The Mozilla Fellowships for Science present a unique opportunity for researchers who want to influence the future of open science and data sharing within their communities.</p>
               <a href="#" className="btn">Learn More</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="event-study-cta row">
+          <div className="col-md-6">
+            <div className="row">
+              <img src="/assets/img/home-events@2x.jpg"/>
+              <div className="col-xs-10 col-xs-push-1">
+                <h3>Events</h3>
+                <p>We offer a series of sprints and workshops that facilitate in-person collaboration and remote contribution to open source projects. We also host regular Community and Project Calls that highlight what the community is up to.</p>
+                <a className="btn m-b-2" href="#TODO">Find An Event</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="row">
+              <img src="/assets/img/home-studygroups@2x.jpg"/>
+              <div className="col-xs-10 col-xs-push-1">
+                <h3>Study Groups</h3>
+                <p>Connect with like-minded researchers at a regular, recurring Study Group session on your campus. If there’s not a Study Group on your campus, help start one!</p>
+                <a className="btn m-b-2" href="#TODO">Join A Group</a>
+              </div>
             </div>
           </div>
         </div>
@@ -65,7 +88,6 @@ export default React.createClass({
               </div>
             </div>
           </div>
-
         </div>
       </div>
     );
