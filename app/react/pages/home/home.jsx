63b1cc8af8f4f15be555ce05de80b81bfa889989
Gavin Lazar Suntop <gavin@gsuntop.com>
2016-04-21 14:48:06 -0700

    sponsor block

--- app/react/pages/home/home.jsx
+++ app/react/pages/home/home.jsx
@@ -57,8 +57,8 @@ export default React.createClass({
           </div>
         </div>
 
-        <div className="whats-new p-y-3 container-dynamic">
-          <h3>{`What's New`}</h3>
+        <div className="whats-new container-dynamic m-t-0">
+          <h3 className="m-y-0 p-y-2">{`What's New`}</h3>
 
           <div className="row p-x-2">
             <div className="col-xs-12 col-lg-4 m-b-2">
@@ -88,6 +88,32 @@ export default React.createClass({
                   <h6 className="m-b-0">Blog</h6>
                   <h4 className="m-y-1">Global Sprint 2016 – Site and Project Submissions Open!</h4>
                   <p>{`Join us as we collaborate on projects helping further science on the open web! We’re back with our third annual global sprint – June 2-3, 2016. We’ve just opened our call for project ideas and site hosts ...`}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="supporters p-t-3">
+          <div className="container-dynamic">
+            <h4 className="text-xs-center m-b-3 m-t-0">We are supported by</h4>
+
+            <div className="row">
+              <div className="col-xs-6 m-b-3">
+                <div className="row">
+                  <div className="col-xs-12 col-sm-4">
+                    <img src="/assets/img/logo-sloan@2x.jpg"/>
+                  </div>
+                  <p className="col-sm-8 hidden-xs-down">{`The Alfred P. Sloan Foundation is a philanthropic, not-for-profit grantmaking institution. They believe that a carefully reasoned and systematic understanding of the forces of nature and society can lead to a better world for all.`}</p>
+                </div>
+              </div>
+
+              <div className="col-xs-6">
+                <div className="row">
+                  <div className="col-xs-12 col-sm-4">
+                    <img src="/assets/img/logo-helmsley@2x.jpg"/>
+                  </div>
+                  <p className="col-sm-8 hidden-xs-down">{`The Helmsley Charitable Trust aspires to improve lives by supporting exceptional nonprofits and other mission-aligned organizations in the U.S. and around the world in health, selected place-based initiatives, and education and human services.`}</p>
                 </div>
               </div>
             </div>
