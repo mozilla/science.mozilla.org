3192e181556a7b02e08aa04392fea40fb49678a8
Alan Mooiman <amooiman@gmail.com>
2016-04-28 20:09:25 -0400

    Fixes

--- app/react/pages/project-details/project-details.jsx
+++ app/react/pages/project-details/project-details.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import GithubAvatar from "../../components/github-avatar/github-avatar.jsx";
 import ProjectCardFeatured from "../../components/project-card-featured/project-card-featured.jsx";
+import Service from "../../../js/backend.js";
 
 export default React.createClass({
 
@@ -14,21 +15,10 @@ export default React.createClass({
     this.getProjectDetails();
   },
   getProjectDetails() {
-    let xhr = new XMLHttpRequest();
-    let url = `https://api-mozillascience-staging.herokuapp.com/projects/${this.props.params.id}/?format=json`;
-
-    xhr.open(`GET`, url);
-    xhr.responseType = `json`;
-
-    xhr.onload = () => {
-      this.setState({projectDetails: xhr.response});
-    };
-
-    xhr.onerror = () => {
-      console.log(`Error fetching projects`);
-    };
-
-    xhr.send();
+    Service.project
+      .get(this.props.params.id)
+      .then((data) => { this.setState({projectDetails: data}); })
+      .catch((reason) => { console.error(reason); });
   },
   render() {
     var project = this.state.projectDetails;
@@ -50,7 +40,7 @@ export default React.createClass({
             <h3>What we're doing</h3>
             <p>{project.description}</p>
             <div className="text-xs-center p-t-1 p-b-3">
-              <a href="#" className="btn visit-repo">VISIT REPO</a>
+              <a href="#" className="btn visit-repo">Visit Repo</a>
             </div>
           </div>
         </div>
