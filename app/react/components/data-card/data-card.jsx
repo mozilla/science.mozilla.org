54d39e62eaef157c4b7ffa46a9dc6745d9f075f6
Alan Mooiman <amooiman@gmail.com>
2016-04-29 11:37:06 -0700

    Refactor project card/data card to be more useful

---
+++ app/react/components/data-card/data-card.jsx
@@ -0,0 +1,32 @@
+
+import React from "react";
+
+export default React.createClass({
+  propTypes: {
+    className: React.PropTypes.string,
+    showPicture: React.PropTypes.bool,
+    picture: React.PropTypes.string,
+    categories: React.PropTypes.arrayOf(React.PropTypes.string)
+  },
+  getDefaultProps() {
+    return{
+      showPicture: false
+    };
+  },
+  render() {
+
+    return (
+      <div className={`data-card-wrapper ` + this.props.className}>
+        <div className="data-card">
+          {this.props.showPicture ? <img className="card-image" src={this.props.picture} /> : null }
+          <div className="categories">
+            {this.props.categories.map((category) => {
+              return <span key={category} className="category-tag">{category}</span>;
+            }) }
+          </div>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+});
--- app/react/components/project-list/project-list.jsx
+++ app/react/components/project-list/project-list.jsx
@@ -1,21 +1,50 @@
 import React from "react";
-import ProjectCard from "../../components/project-card/project-card.jsx";
+import {Link} from "react-router";
+import DataCard from "../../components/data-card/data-card.jsx";
+import GithubAvatar from "../../components/github-avatar/github-avatar.jsx";
 
 
 export default React.createClass({
   propTypes: {
-    project: React.PropTypes.array
+    projects: React.PropTypes.array
   },
   getDefaultProps() {
     return {
       projects: []
     };
   },
+  formatTags(project) {
+    let tags = null;
+
+    if (project.tags) {
+      tags = (<div className="help-with p-y-1">Help with:&nbsp;
+        {project.tags.map((tag)=> {
+          return <span className="project-tag" key={tag}><Link to="#">{tag}</Link></span>;
+        })}
+      </div>
+      );
+    }
+    return tags;
+  },
   render() {
     return(
       <div className="row">
         {this.props.projects.map((project) => {
-          return <ProjectCard key={project.id} className="col-md-4" isFeatured={false} project={project}/>;
+          return (
+            <DataCard key={project.id} className="col-md-4" showPicture={false} categories={project.categories}>
+              <div className="profile-photos">
+                <GithubAvatar user={project.github_contributors[project.github_contributors.length-1]} />
+              </div>
+              <div>
+                <Link to="#" className="names">User Name (awaiting API)</Link>
+                <span> for </span>
+                <span className="institution">{project.institution}</span>
+              </div>
+              <Link to={`projects/${project.id}`} className="project-name m-t-1">{project.name}</Link>
+              <div className="project-description m-t-1">{project.short_description}</div>
+              {this.formatTags(project)}
+            </DataCard>
+          );
         })}
       </div>
     );
