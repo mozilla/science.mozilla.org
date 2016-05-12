aee00ec5ea9425f03e4d3a5d4e98441745456a1a
Alan Mooiman <amooiman@gmail.com>
2016-05-12 10:38:22 -0700

    Code of Conduct (#354)

---
+++ app/react/pages/code-of-conduct/code-of-conduct.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default React.createClass({
+  componentDidMount() {
+
+  },
+  render() {
+    return (
+      <div id="code-of-conduct">
+        <div className="jumbotron container text-xs-center m-b-0 p-b-1">
+          <h1>Code of Conduct</h1>
+          <div className="row">
+            <p className="lead col-xs-12 col-md-8 col-md-push-2">Mozilla Science Lab events are community events intended for networking and collaboration as well as learning.</p>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12 col-md-8 col-md-push-2 m-b-1">
+              <p>We value the participation of every member of the scientific community and want all attendees to have an enjoyable and fulfilling experience. Accordingly, all attendees are expected to show respect and courtesy to other attendees throughout the event and in interactions online associated with the Science Lab.</p>
+              <p>To make clear what is expected, everyone taking part in Science Lab events and discussions—instructors, helpers, organizers, and learners—is required to conform to the following Code of Conduct. Organizers will enforce this code throughout events, but you may also contact us directly by email at<a href="mailto:sciencelab@mozillafoundation.org"> sciencelab@mozillafoundation.org</a>. All communication will be treated as confidential.</p>
+              <h2>The Short Version</h2>
+              <ul>
+                <li>The Mozilla Science Lab is dedicated to providing a harassment-free learning experience for everyone, regardless of gender, sexual orientation, disability, physical appearance, body size, race, religion, or choice of text editor. We do not tolerate harassment of participants in any form.</li>
+                <li>All communication should be appropriate for a professional audience including people of many different backgrounds. Sexual language and imagery is not appropriate for any event.</li>
+                <li>Be kind to others. Do not insult or put down other attendees.</li>
+                <li>Behave professionally. Remember that harassment and sexist, racist, or exclusionary jokes are not appropriate.</li>
+                <li>Attendees violating these rules may be asked to leave the event at the sole discretion of the conference organizers without a refund of any charge that may have been levied.</li>
+              </ul>
+              <p>Thank you for helping make this a welcoming, friendly event for all.</p>
+              <h2>The Longer Version</h2>
+              <p>Harassment includes offensive verbal comments related to gender, sexual orientation, disability, physical appearance, body size, race, religion, sexual images in public spaces, deliberate intimidation, stalking, following, harassing photography or recording, sustained disruption of talks or other events, inappropriate physical contact, and unwelcome sexual attention.</p>
+              <p>Participants asked to stop any harassing behavior are expected to comply immediately.</p>
+              <p>Be careful in the words that you choose. Remember that sexist, racist, and other exclusionary jokes can be offensive to those around you.</p>
+              <p>If a participant engages in behavior that violates this code of conduct, the organizers may take any action they deem appropriate, including warning the offender or expulsion from the event with no refund of any fee that may have been levied.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
