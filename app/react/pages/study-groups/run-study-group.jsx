ef4f430fd92122641d7cb0ed4e8080be01ef5e68
Alan Mooiman <amooiman@gmail.com>
2016-05-13 16:17:15 -0700

    Fixups

--- app/react/pages/study-groups/run-study-group.jsx
+++ app/react/pages/study-groups/run-study-group.jsx
@@ -4,20 +4,47 @@ import DataCard from "../../components/data-card/data-card.jsx";
 
 export default React.createClass({
   componentWillMount() {
+    this.loadStudyGroups(1);
+  },
+  loadStudyGroups: function(page) {
     Service.resources
-      .get({format: `json`, tags: `Study Group`})
-      .then((data) => { this.setState({lessons:data.results}); })
+      .get({format: `json`, tags: `Study Group`, page})
+      .then((data) => {
+        this.setState({
+          lessons:data.results,
+          pagesLoaded: page,
+          allPagesLoaded: !data.next
+        });
+      })
       .catch((reason) => { console.error(reason); });
   },
   getInitialState() {
     return {
-      lessons: []
+      lessons: [],
+      pagesLoaded: 0,
+      allPagesLoaded: false
     };
   },
   onFormSubmit() {
     // TODO: Make this not a popup
   },
+  onMoreClick: function () {
+    this.loadResources(this.state.pagesLoaded + 1);
+  },
   render() {
+    let studyLessons = [];
+
+    if (this.state.lessons.length) {
+      studyLessons = this.state.lessons.map((lesson) => {
+        return (
+          <DataCard key={lesson.id} className="col-xs-12 col-sm-6 col-lg-4" showPicture={false} categories={lesson.tags.filter((tag)=>{ return tag !== `Study Group`; })}>
+              <div className="m-y-1"><a className="project-name" href="#">{lesson.name}</a></div>
+              <p>{lesson.description}</p>
+          </DataCard>
+        );
+      });
+    }
+
     return (
       <div className="container">
         <div className="row">
@@ -57,15 +84,11 @@ export default React.createClass({
             <h3>Study Group Lessons</h3>
             <p>The Study Group Lessons are a great place to find existing curriculum in open science, open code, as well as creative approaches to research review and workflows. Lessons are stored in our <a href="https://github.com/mozillascience/studyGroupLessons">Lessons bank on github</a>, indexed in <a href="https://mozillascience.github.io/studyGroupHandbook/lessons.html">our handbook</a>, and mirrored on our website. Check out the <a href="https://github.com/mozillascience/studyGroupLessons/issues">issues in the Lessons bank</a> for more ideas and versions of the core lessons, and our <a href="https://www.youtube.com/playlist?list=PLyfPqolZyFtyE8zx2Dbm4uqZYQIeL9D9Z">online webcasts for remote events</a>!</p>
             <div className="row">
-            {this.state.lessons.map((lesson) => {
-              return (
-                <DataCard key={lesson.id} className="col-xs-12 col-sm-6 col-lg-4" showPicture={false} categories={lesson.tags.filter((tag)=>{ return tag !== `Study Group`; })}>
-                    <div className="m-y-1"><a className="project-name" href="#">{lesson.name}</a></div>
-                    <p>{lesson.description}</p>
-                </DataCard>
-              );
-            })}
+            {studyLessons}
             </div>
+            <div className="text-xs-center">
+              <button hidden={this.state.allPagesLoaded} className="btn m-b-3" onClick={this.onMoreClick}>See More</button>
+          </div>
           </div>
         </div>
       </div>
