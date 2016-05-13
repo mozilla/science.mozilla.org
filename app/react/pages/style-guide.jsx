3ee742a00f3f142ca81fb8e85d9fc58653dbd714
gavin lazar suntop <gavin@gsuntop.com>
2016-05-13 13:07:33 -0700

    Creating RadioFilter component and using it on Projects (#369)

--- app/react/pages/style-guide.jsx
+++ app/react/pages/style-guide.jsx
@@ -2,10 +2,39 @@ import React from "react";
 import ThreeUp from "../components/three-up/three-up.jsx";
 import TabSwitcher from "../components/tab-switcher/tab-switcher.jsx";
 import Collapse from "rc-collapse";
+import RadioFilter from "../components/radio-filter/radio-filter.jsx";
+
 let Panel = Collapse.Panel;
 
 export default React.createClass({
   render() {
+    let radioOptions = [
+      {
+        value: `featured`,
+        label: `Featured Projects`
+      },
+      {
+        value: `date_updated`,
+        label: `Recently Updated`
+      },
+      {
+        value: `date_created`,
+        label: `Recently Added`
+      },
+      {
+        value: `active`,
+        label: `Most Active`
+      },
+      {
+        value: `contributors`,
+        label: `Most Contributors`
+      }
+    ];
+
+    function handleRadioChange(choice) {
+      console.log(`radioChange: ${choice}`);
+    }
+
     return (
       <div className="container-dynamic">
         <h1>Style Guide</h1>
@@ -49,12 +78,17 @@ export default React.createClass({
         </div>
 
         <h3>Accordion</h3>
+
         <div className="m-b-1">
           <Collapse accordion={false}>
             <Panel header="Heading 1">Forage flexitarian salvia migas fashion axe, meggings locavore poutine. Lo-fi plaid PBR&B, umami pinterest swag authentic beard cold-pressed. Ennui selfies scenester, kickstarter raw denim ramps disrupt forage keffiyeh put a bird on it. Direct trade helvetica umami messenger bag echo park typewriter chicharrones, williamsburg iPhone polaroid offal retro marfa. Offal cronut disrupt banh mi, kitsch shabby chic deep v schlitz intelligentsia letterpress affogato kogi. Green juice tacos austin gochujang, chillwave food truck chambray 8-bit master cleanse forage paleo bespoke. Yr squid ethical irony kickstarter, man braid paleo salvia man bun cred ugh tote bag post-ironic.</Panel>
             <Panel header="Heading 2">Forage flexitarian salvia migas fashion axe, meggings locavore poutine. Lo-fi plaid PBR&B, umami pinterest swag authentic beard cold-pressed. Ennui selfies scenester, kickstarter raw denim ramps disrupt forage keffiyeh put a bird on it. Direct trade helvetica umami messenger bag echo park typewriter chicharrones, williamsburg iPhone polaroid offal retro marfa. Offal cronut disrupt banh mi, kitsch shabby chic deep v schlitz intelligentsia letterpress affogato kogi. Green juice tacos austin gochujang, chillwave food truck chambray 8-bit master cleanse forage paleo bespoke. Yr squid ethical irony kickstarter, man braid paleo salvia man bun cred ugh tote bag post-ironic.</Panel>
           </Collapse>
         </div>
+
+        <h3>SortFilter</h3>
+
+        <RadioFilter options={radioOptions} initialChoice={`date_created`} onChange={handleRadioChange}></RadioFilter>
 
       </div>
     );
