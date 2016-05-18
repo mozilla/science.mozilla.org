37ddff83a3cb97e293ebab87cd8f055cfc34f9d1
Alan Mooiman <amooiman@gmail.com>
2016-05-18 13:05:33 -0700

    Paginate events
    Closes #387

--- app/react/pages/events.jsx
+++ app/react/pages/events.jsx
@@ -4,29 +4,39 @@ import Service from "../../js/backend.js";
 
 export default React.createClass({
   componentWillMount() {
-    this.getEventList();
+    this.getEvents(`future`, 1);
+    this.getEvents(`past`, 1);
   },
-  getEventList() {
+  getEvents(tense, page) {
     Service.events
       .get({
-        filter: `future`,
-        format: `json`
+        filter: tense,
+        format: `json`,
+        page
       })
-      .then((events) => { this.setState({futureEvents: events.results}); })
-      .catch((reason) => { console.error(reason); });
-
-    Service.events
-      .get({
-        filter: `past`,
-        format: `json`
+      .then((data) => {
+        this.setState(state => {
+          state.events[tense] = state.events[tense].concat(data.results);
+          state.allEventsLoaded[tense] = !data.next;
+          state.pageLoaded[tense] = page;
+        });
       })
-      .then((events) => { this.setState({pastEvents: events.results}); })
       .catch((reason) => { console.error(reason); });
   },
   getInitialState() {
     return {
-      futureEvents: [],
-      pastEvents: []
+      events: {
+        past: [],
+        future: []
+      },
+      allEventsLoaded: {
+        past: false,
+        future: false
+      },
+      pageLoaded: {
+        past: 0,
+        future: 0
+      }
     };
   },
   render() {
@@ -37,13 +47,19 @@ export default React.createClass({
           <p className="lead m-t-1">We offer a series of global and local sprints that facilitate in-person collaboration, and remote contribution to open source projects. We also host regular Community Calls and Project Calls that highlight what the Mozilla Science Lab community is up to.</p>
         </div>
         <div className="container-dynamic">
-          <EventList events={this.state.futureEvents} />
+          <EventList events={this.state.events.future} />
+          <div className="text-xs-center">
+            <button hidden={this.state.allEventsLoaded.future} className="btn m-b-3" onClick={()=>{ this.getEvents(`future`, this.state.pageLoaded.future + 1); }}>See More</button>
+          </div>
         </div>
         <div className="jumbotron container text-xs-center m-b-0 p-b-1">
           <h2>Archive of Past Events</h2>
         </div>
         <div className="container-dynamic">
-          <EventList cardClass="col-sm-6 col-md-4 archive" pictures={false} events={this.state.pastEvents} />
+          <EventList cardClass="col-sm-6 col-md-4 archive" pictures={false} events={this.state.events.past} />
+          <div className="text-xs-center">
+            <button hidden={this.state.allEventsLoaded.past} className="btn m-b-3" onClick={()=>{ this.getEvents(`past`, this.state.pageLoaded.past + 1); }}>See More</button>
+          </div>
         </div>
       </div>
     );
