4c949d8e0c593d7d2ca5f403ad499297453ceb73
Akshat Garg <coolakshat24@gmail.com>
2017-04-18 19:42:11 +0530

    GithubAvatar: Make changes for es6 migrations
    See #613

--- app/react/components/github-avatar/github-avatar.jsx
+++ app/react/components/github-avatar/github-avatar.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 
-export default React.createClass({
-  propTypes: {
-    user: React.PropTypes.object.isRequired,
-    size: React.PropTypes.number
-  },
-  getDefaultProps() {
-    return {
-      size: 40
-    };
-  },
+export default class GithubAvatar extends React.Component {
+
   render() {
+
     return (
       <a className="github-avatar" href={`https://github.com/${this.props.user.github_username}`}>
         <img className="circle" srcSet={`${this.props.user.avatar_url}&s=${this.props.size}, ${this.props.user.avatar_url}&s=${this.props.size*2} 2x`} width={this.props.size} height={this.props.size} alt={this.props.user.name} />
       </a>
     );
   }
-});
+}
+
+GithubAvatar.propTypes = {
+  user: React.PropTypes.object.isRequired,
+  size: React.PropTypes.number
+};
+
+GithubAvatar.defaultProps = {
+  size: 40
+};
