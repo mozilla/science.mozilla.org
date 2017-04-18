fa449f0364aa553870e7f96ec87e14c9dc104660
Akshat Garg <coolakshat24@gmail.com>
2017-04-18 20:07:47 +0530

    Header: Make changes for es6 migrations
    See #613

--- app/react/components/header/header.jsx
+++ app/react/components/header/header.jsx
@@ -3,38 +3,43 @@ import { Link } from "react-router";
 import classNames from "classnames";
 import HamburgerMenu from "react-hamburger-menu";
 
-export default React.createClass({
-  propTypes: {
-    path: React.PropTypes.string.isRequired
-  },
-  getInitialState() {
-    return ({
+export default class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
       menuOpen: false,
       slideoutVisible: false
-    });
-  },
-  hideMenu() {
+    };
+  }
+
+  hideMenu = () => {
     this.setState({
       menuOpen: false,
       slideoutVisible: false
     });
     window.removeEventListener(`click`, this.hideMenu);
-  },
-  toggleMenu(e) {
+  }
+
+  toggleMenu = (e) => {
     if(!this.state.menuOpen){
       e.stopPropagation();
       this.setState({ menuOpen: !this.state.menuOpen });
       window.addEventListener(`click`, this.hideMenu);
     }
-  },
-  toggleSlideout(e) {
+  }
+
+  toggleSlideout = (e) => {
     if(!this.state.slideoutVisible){
       e.stopPropagation();
       this.setState({ slideoutVisible: !this.state.slideoutVisible });
       window.addEventListener(`click`, this.hideMenu);
     }
-  },
-  render: function() {
+  }
+
+  render() {
+
     let slideoutNavClass = classNames({
       "slideout-menu": true,
       active: this.state.slideoutVisible
@@ -97,4 +102,8 @@ export default React.createClass({
       </header>
     );
   }
-});
+}
+
+Header.propTypes = {
+  path: React.PropTypes.string.isRequired
+};
