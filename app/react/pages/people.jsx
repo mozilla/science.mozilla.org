ebaddc010ad9c9a8922ca7cfc5266d6c77fec0ce
auremoser <auremoser@gmail.com>
2017-05-24 16:55:25 -0700

    update people pages with contemp bios

--- app/react/pages/people.jsx
+++ app/react/pages/people.jsx
@@ -14,14 +14,14 @@ var people = [
     title: `Learning Strategist`,
     image: `https://avatars3.githubusercontent.com/u/13855390?v=3&s=400`,
     links: [<a href="https://twitter.com/zannahlou">@zannahlou</a>],
-    bio: `Zannah draws on her background in interaction design, project-based learning, visual art, and storytelling to create “sticky” learning experiences around technology and design. She's taught web design, programming, interaction design, and data visualization at NYU, the New School, and in the City University of New York system. She was Senior Content Developer at the interactive design firm Local Projects, and an exhibit developer for the Museum of Science in Boston. In her spare time Zannah draws mini-comics and rides her bike around Brooklyn.`
+    bio: `Zannah draws on her background in interaction design, project-based learning, visual art, and storytelling to create “sticky” learning experiences around technology and design. She's taught web design, programming, interaction design, and data visualization at NYU, the New School, and in the City University of New York system. She was Senior Content Developer at the interactive design firm Local Projects, and an exhibit developer for the Museum of Science in Boston. In her spare time Zannah draws mini-comics and rides her skateboard around Gainesville, Florida.`
   },
   {
     name: `Aurelia Moser`,
     title: `Community Lead`,
     image: `https://avatars3.githubusercontent.com/u/1559703?v=3&s=400`,
     links: [<a href="https://twitter.com/auremoser">@auremoser</a>],
-    bio: `Aurelia is a creative developer building community around code at the Science Lab. Previously of Ushahidi, Internews-Kenya, and CartoDB, she has a background that blends a cocktail of conservation chemistry and coding for civic tech/non-profit journalism. Recent projects have had mapping sensor data to support agricultural security and sustainable apis ecosystems in the Global South, though she also dabbles in DJing and privacy art. Ask her about yoga, semantic web theory, web-mapping, and organic chem.`
+    bio: `Aurelia is a creative developer building community around code at the Science Lab. Previously of Ushahidi, Internews-Kenya, and Carto, she has a background in a cocktail of conservation chemistry and coding for civic tech/non-profits. Recent projects have had mapping sensor data to support agricultural security and sustainable apis ecosystems in the Global South, though she also dabbles in DJing and bio-privacy art. Ask her about yoga, web-cartography, and organic chem.`
   }
 ];
 
